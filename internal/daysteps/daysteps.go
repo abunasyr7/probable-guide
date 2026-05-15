8c0bc116c1f070fcfc8e337c9fb0dd931d78f830
Abunasyr <abunasyr1@gmail.com>
2026-05-15 15:52:17 +0500

    feat: добавил испонение функции parsePackage

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,6 +1,9 @@
 package daysteps
 
 import (
+	"fmt"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -12,7 +15,25 @@ const (
 )
 
 func parsePackage(data string) (int, time.Duration, error) {
-	// TODO: реализовать функцию
+	parts := strings.Split(data, ",")
+
+	if len(parts) < 2 {
+		return  0, 0, fmt.Errorf("invalid format: %s", data)
+	}
+
+	num, err := strconv.Atoi(parts[0])
+
+	if err != nil {
+		return 0, 0, fmt.Errorf("invalid int: %w", err)
+	}
+
+	duration, err := time.ParseDuration(parts[1])
+
+	if err != nil {
+		return 0, 0, fmt.Errorf("invalid duration %w", err)
+	}
+
+	return num, duration, nil
 }
 
 func DayActionInfo(data string, weight, height float64) string {
