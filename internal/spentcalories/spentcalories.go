99c5debb4846def259f9c2764aee6cf9ea44a76a
Abunasyr <abunasyr1@gmail.com>
2026-05-15 18:34:56 +0500

    feat: добавил выполнение программы meanSpeed

--- internal/spentcalories/spentcalories.go
+++ internal/spentcalories/spentcalories.go
@@ -45,7 +45,14 @@ func distance(steps int, height float64) float64 {
 }
 
 func meanSpeed(steps int, height float64, duration time.Duration) float64 {
-	// TODO: реализовать функцию
+	if duration <= 0 {
+		return 0
+	}
+
+	distanceData := distance(steps, height)
+	hours := duration.Hours()
+	
+	return distanceData / hours
 }
 
 func TrainingInfo(data string, weight, height float64) (string, error) {
